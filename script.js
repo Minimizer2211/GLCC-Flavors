b25b5592f308e69206b28f49cc7e6d1ed1351d93
Minimizer2211 <161833931+Minimizer2211@users.noreply.github.com>
2024-05-02 07:20:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const html = document.documentElement;
+const frameCounts = { ".bgimg-1": 126, ".bgimg-2": 126, ".bgimg-3": 126, ".bgimg-4": 126 };
+const baseUrls = { ".bgimg-1": "./", ".bgimg-2": "./", ".bgimg-3": "./", ".bgimg-4": "./" };
+const scrollOffsets = { ".bgimg-1": 0, ".bgimg-2": 0, ".bgimg-3": 0, ".bgimg-4": 0 }; // Adjust these values as needed
+
+const preloadImages = (selector) => {
+  const frameCount = frameCounts[selector];
+  const baseUrl = baseUrls[selector];
+  for (let i = 1; i < frameCount; i++) {
+    const frameSrc = `${baseUrl}${i.toString().padStart(4, '0')}.webp`;
+    const img = new Image();
+    img.src = frameSrc;
+  }
+};
+
+const updateImage = (selector, index) => {
+  const frameCount = frameCounts[selector];
+  const baseUrl = baseUrls[selector];
+  const frameSrc = `${baseUrl}${index.toString().padStart(4, '0')}.webp`;
+  const bgImgElement = document.querySelector(selector);
+  bgImgElement.style.backgroundImage = `url(${frameSrc})`;
+};
+
+const onScroll = () => {
+  for (const selector in frameCounts) {
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = (scrollTop / maxScrollTop) - scrollOffsets[selector]; // Adjusted scroll position with offset
+    let frameIndex = Math.min(
+      frameCounts[selector] - 1,
+      Math.ceil(scrollFraction * frameCounts[selector])
+    );
+
+    // Starting from the second frame
+    frameIndex = Math.max(frameIndex, 1);
+
+    updateImage(selector, frameIndex);
+  }
+};
+
+window.addEventListener('scroll', onScroll);
+
+for (const selector in frameCounts) {
+  preloadImages(selector);
+}
